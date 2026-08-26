46392b2839e6575e7f721af8e13bf7874855f6d2
SakshyamStha <sakshamstha6785@gmail.com>
2026-08-26 23:10:05 +0545

    update: updated app component to host the routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/header/header';
+import { FooterComponent } from './layouts/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('sid-cafe');
