faf3779bca8d5edb3486d5ada183190cb19db5cf
SakshyamStha <sakshamstha6785@gmail.com>
2026-08-28 17:59:34 +0545

    feat: api endpoint folder for endpoints

---
+++ src/app/api- configs/api-endpoints.ts
@@ -0,0 +1,9 @@
+import { environment } from '../../environments/environment';
+
+export const API_ENDPOINTS = {
+  LOGIN: `${environment.apiUrl}v1/user/login`,
+
+  //   reservation
+
+  RESERVATION_ADD: `${environment.apiUrl}reservations/`,
+};
