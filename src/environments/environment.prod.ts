2c3defb9ad6f26ff412d5f34206dd99d35c7504f
SakshyamStha <sakshamstha6785@gmail.com>
2026-09-10 23:50:23 +0545

    env prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,7 @@
 //   apiUrl: 'http://127.0.0.1:8000/api/',
 // };
 
-
 export const environment = {
   production: true,
-  apiUrl: 'https://your-cms-domain.com/api/',
+  apiUrl: '/assets/data/',
 };
